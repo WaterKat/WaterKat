5f49a842380fb6fe17be706282f9fe446489c40f
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-08 12:43:03 -0800

    Added default quitting logic.

--- GameAssets/SceneManagement/Scenes/Quit.cs
+++ GameAssets/SceneManagement/Scenes/Quit.cs
@@ -2,5 +2,12 @@
 
 namespace WaterKat.GameAssets.SceneManagement.Scenes
 {
-    public class Quit : Scene { }
+    public class Quit : Scene 
+    {
+        public override Scene Update()
+        {
+            Console.WriteLine("Quitting...");
+            return this;
+        }
+    }
 }
