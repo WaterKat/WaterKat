7bc627f77941c024bb74b5e61205bf4e46328f89
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-08 16:01:08 -0800

    Added notice of unimplemented Quit override

--- GameAssets/SceneManagement/Scenes/Quit.cs
+++ GameAssets/SceneManagement/Scenes/Quit.cs
@@ -6,6 +6,7 @@ namespace WaterKat.GameAssets.SceneManagement.Scenes
     {
         public override Scene Update()
         {
+            Console.WriteLine("Unimplemented Quit Sequence...");
             Console.WriteLine("Quitting...");
             return this;
         }
