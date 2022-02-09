a30d375eac2a3b6100bb04204bfa3d675f0c4745
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-08 16:00:25 -0800

    Added abstraction for input and output between the application and the console with the IO class

---
+++ DungeonRun/IO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterKat.DungeonRun
+{
+    public static class IO
+    {
+        public static void Println(string _string)
+        {
+            Console.WriteLine(_string);
+        }
+        public static void Print(string _string)
+        {
+            Console.Write(_string);
+        }
+        public static string? GetInput()
+        {
+            return Console.ReadLine();
+        }
+        public static void Pause()
+        {
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
