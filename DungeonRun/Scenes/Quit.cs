32b1a3f3a954feac230ba6b2a106065ef5cde098
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-08 16:03:41 -0800

    Minor Quit implementation

--- DungeonRun/Scenes/Quit.cs
+++ DungeonRun/Scenes/Quit.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WaterKat.GameAssets.SceneManagement;
 
 namespace WaterKat.DungeonRun.Scenes
 {
     public class Quit : WaterKat.GameAssets.SceneManagement.Scenes.Quit
     {
+        public override Scene Update()
+        {
+            Console.WriteLine("Thanks for playing!");
+            Console.WriteLine("Quitting...");
+            return this;
+        }
     }
 }
