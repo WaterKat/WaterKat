19d958396b656edb3114c5d25549f44ef8efca30
Jacob Vallecillo <jacob.vallecillo@gmail.com>
2022-02-14 22:08:27 -0800

    Added slot selection to load game

--- DungeonRun/SceneManagement/Scenes/LoadGame.cs
+++ DungeonRun/SceneManagement/Scenes/LoadGame.cs
@@ -8,18 +8,43 @@ namespace WaterKat.DungeonRun.SceneManagement.Scenes
         {
             IO.Println("You wish to load a new adventure?");
             int availableSlot = 0;
+            List<int> slots = new List<int>();
             while (SaveManagement.SaveManager.SaveFileExists(availableSlot))
             {
                 GameData gameData;
                 bool loadSuccess = SaveManagement.SaveManager.LoadData(availableSlot, out gameData);
+                if (loadSuccess)
+                {
+                    IO.Println("[" + availableSlot.ToString() + "] " + gameData.WorldName);
+                    slots.Add(availableSlot);
+                    availableSlot++;
+                }
+            }
+            slots.Add(-1);
 
-                IO.Println("[" + availableSlot.ToString() + "] " + gameData.WorldName);
-                availableSlot++;
+            int desiredSlot = IO.AskForInt("Please select a save slot.\nSlot: ", slots.ToArray<int>());
+            if (desiredSlot !=-1)
+            {
+                bool loadSuccess = SaveManager.LoadData(desiredSlot, out sceneManager.GameData);
+                if (!loadSuccess)
+                {
+                    IO.Println("Uh Oh, there was an error loading you save.");
+                    IO.Println();
+                    return previousScene ?? new Title();
+                }
+                else
+                {
+                    IO.Println("Your game was successfully loaded");
+                    IO.Println();
+                }
+            }
+            else
+            {
+                IO.Println("Canceling load, going back to Menu");
+                return previousScene ?? new Title();
             }
 
-
             IO.Println();
-
 
             return  new Introduction();
         }
