9e227798f19fa7320c2ce586781aea09d8ce80f5
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-14 17:38:21 -0800

    Added new DR specific scene class with the goal to refactor

---
+++ DungeonRun/SceneManagement/Scene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterKat.DungeonRun.SceneManagement
+{
+    public abstract class Scene : WaterKat.GameAssets.SceneManagement.Scene
+    {
+        public new DRSceneManager? sceneManager = null;
+        public new Scene? previousScene = null;
+    }
+}
