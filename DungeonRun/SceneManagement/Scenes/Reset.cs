cdb525363edb3106cda8d970e9175d8ab820b7a9
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-16 11:49:45 -0800

    Added reset message

--- DungeonRun/SceneManagement/Scenes/Reset.cs
+++ DungeonRun/SceneManagement/Scenes/Reset.cs
@@ -8,5 +8,13 @@ namespace WaterKat.DungeonRun.SceneManagement.Scenes
 {
     public class Reset : Scene
     {
+        public override Scene Update()
+        {
+            IO.Println("Catastrophic failure. Resetting.");
+            IO.Println();
+            IO.Println();
+            IO.Println();
+            return this;
+        }
     }
 }
