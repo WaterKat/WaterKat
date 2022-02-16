4aa368ff507e0365efb567c066ac6757f6821256
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-15 23:02:05 -0800

    Added more data prompts to Introduction to fill in game data.

--- DungeonRun/SceneManagement/Scenes/Introduction.cs
+++ DungeonRun/SceneManagement/Scenes/Introduction.cs
@@ -6,15 +6,25 @@ namespace WaterKat.DungeonRun.SceneManagement.Scenes
     {
         public override Scene Update()
         {
+            if (sceneManager == null)
+                return new Title();
+
             string input;
             bool inputIsCorrect;
 
-            input = IO.AskForStringWithConfirm("You, a lone adventurer, sit near a campfire, your name is ... ", true);
-            sceneManager.GameData.playerCharacter.MetaData.Name = input;
+            string worldName = IO.AskForStringWithConfirm("In the a great world named... ", true);
+            sceneManager.GameData.WorldName = worldName;
 
+            string characterName = IO.AskForStringWithConfirm("You, a lone adventurer, sit near a campfire. Your name is ... ", true);
+            sceneManager.GameData.playerCharacter.MetaData.Name = characterName;
+            
+            string goalLocationName = IO.AskForStringWithConfirm("And you are making your way to... ", true);
+            sceneManager.GameData.GoalLocationName = goalLocationName;
 
+            string goalItemName = IO.AskForStringWithConfirm("Once there you will find your prized... ", true);
+            sceneManager.GameData.GoalLocationName = goalItemName;
 
-            return this;
+            return new SaveMenu(new Level_0());
         }
     }
 }
