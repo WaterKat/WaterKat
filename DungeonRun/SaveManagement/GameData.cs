6690d93ca8d7e8c57196dc06eba98d5677fb6ff6
WATERKATROG\jacobv <jacob.vallecillo@gmail.com>
2022-02-15 23:01:41 -0800

    Added GoalLocationName, GoalItemName, and version fields

--- DungeonRun/SaveManagement/GameData.cs
+++ DungeonRun/SaveManagement/GameData.cs
@@ -5,7 +5,12 @@ namespace WaterKat.DungeonRun.SaveManagement
     [System.Serializable]
     public struct GameData
     {
+        public string version = "0.0.1";
+
         public string WorldName = "Aeliroth";
+        public string GoalLocationName = "Vandel";
+        public string GoalItemName = "Ring";
+
         public PlayerCharacter playerCharacter = new PlayerCharacter();
         public int valueA;
         public int valueB;
